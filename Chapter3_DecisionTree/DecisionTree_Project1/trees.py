1a266a172c1d1a6d3b6ea806f87c4e0de32ac0b0
songyu@eastmoney.com <songyu@eastmoney.com>
2020-05-25 17:57:27 +0800

    决策树代码上传。

--- Chapter3_DecisionTree/DecisionTree_Project1/trees.py
+++ Chapter3_DecisionTree/DecisionTree_Project1/trees.py
@@ -1,4 +1,9 @@
 from math import log
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+import pickle
 
 def calcShannonEnt(dataSet):
     """
@@ -107,5 +112,411 @@ def splitDataSet(dataSet, axis, value):
     return retDataSet
 
 
+def chooseBestFeatureToSplit(dataSet):
+    """
+    函数说明：选择最优特征
+            Gain(D,g) = Ent(D) - SUM(|Dv|/|D|)*Ent(Dv)
+
+    Parameters:
+        dataSet - 数据集
+        
+    Returns:
+        bestFeature - 信息增益最大的（最优）特征的索引值
+        
+    Modify:
+        2020-05-25
+    """
+    # 特征数量
+    numFeatures = len(dataSet[0]) - 1
+    # 计算数据集的香农熵
+    baseEntropy = calcShannonEnt(dataSet)
+    # 信息增益
+    bestInfoGain = 0.0
+    # 最优特征的索引值
+    bestFeature = -1
+    # 遍历所有特征
+    for i in range(numFeatures):
+        # 获取dataSet的第i个所有特征存在featList这个列表中（列表生成式）
+        featList = [example[i] for example in dataSet]
+        # 创建set集合{}，元素不可重复，重复的元素均被删掉
+        # 从列表中创建集合是python语言得到列表中唯一元素值得最快方法
+        uniqueVals = set(featList)
+        # 经验条件熵
+        newEntropy = 0.0
+        # 计算信息增益
+        for value in uniqueVals:
+            # subDataSet划分后的子集
+            subDataSet = splitDataSet(dataSet, i, value)
+            # 计算子集的概率
+            prob = len(subDataSet) / float(len(dataSet))
+            # 根据公式计算经验条件熵
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        # 信息增益
+        infoGain = baseEntropy - newEntropy
+        # 打印每个特征的信息增益
+        print("第%d个特征的增益为%.3f" % (i, infoGain))
+        # 计算信息增益
+        if(infoGain > bestInfoGain):
+            # 更新信息增益，找到最大的信息增益
+            bestInfoGain = infoGain
+            # 记录信息增益最大的特征的索引值
+            bestFeature = i
+    # 返回信息增益最大的特征的索引值
+    return bestFeature
+
+
+# 3.1.3 递归构建决策树
+def majorityCnt(classList):
+    """
+    函数说明：统计classList中出现次数最多的元素（类标签）
+            服务于递归第两个终止条件
+
+    Parameters:
+        classList - 类标签列表
+        
+    Returns:
+        sortedClassCount[0][0] - 出现次数最多的元素（类标签）
+        
+    Modify:
+        2020-05-25
+    """   
+    classCount = {}
+    # 统计classList中每个元素出现的次数
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    # 根据字典的值降序排序
+    # operator.itemgetter(1)获取对象的第1列的值
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
+    # 返回classList中出现次数最多的元素
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels, featLabels=None):
+    """
+    函数说明：创建决策树（ID3算法）
+            递归有两个终止条件：1、所有的类标签完全相同，直接返回类标签
+                            2、用完所有标签但是得不到唯一类别的分组，即特征不够用，挑选出现数量最多的类别作为返回
+
+    Parameters:
+        dataSet - 训练数据集
+        labels - 分类属性标签
+        featLabels - 存储选择的最优特征标签
+        
+    Returns:
+        myTree - 决策树
+        
+    Modify:
+        2020-05-25
+    """
+    # 取分类标签（是否放贷：yes or no）
+    classList = [example[-1] for example in dataSet]
+    # 如果类别完全相同则停止继续划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 遍历完所有特征时返回出现次数最多的类标签
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    # 选择最优特征
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    # 最优特征的标签
+    bestFeatLabel = labels[bestFeat]
+
+    # 若最优特征标签不存在，则初始化它
+    if featLabels==None:
+        featLabels=[]
+    featLabels.append(bestFeatLabel)
+    # 根据最优特征的标签生成树
+    myTree = {bestFeatLabel:{}}
+    # 删除已经使用的特征标签
+    del(labels[bestFeat])
+    # 得到训练集中所有最优解特征的属性值
+    featValues = [example[bestFeat] for example in dataSet]
+    # 去掉重复的属性值
+    uniqueVals = set(featValues)
+    # 遍历特征，创建决策树
+    for value in uniqueVals:
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels, featLabels)
+    return myTree
+
+
+def getNumLeafs(myTree):
+    """
+    函数说明：获取决策树叶子结点的数目
+
+    Parameters:
+        myTree - 决策树
+        
+    Returns:
+        numLeafs - 决策树的叶子结点的数目
+        
+    Modify:
+        2020-05-25
+    """ 
+    # 初始化叶子
+    numLeafs = 0
+    # python3中myTree.keys()返回的是dict_keys,不是list,所以不能用
+    # myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    # next() 返回迭代器的下一个项目 next(iterator[, default])
+    firstStr = next(iter(myTree))
+    # 获取下一组字典
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        # 测试该结点是否为字典，如果不是字典，代表此节点为叶子结点
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+ 
+def getTreeDepth(myTree):
+    """
+    函数说明：获取决策树的层数
+
+    Parameters:
+        myTree - 决策树
+        
+    Returns:
+        maxDepth - 决策树的层数
+        
+    Modify:
+        2020-05-25
+    """  
+    # 初始化决策树深度
+    maxDepth = 0
+    # python3中myTree.keys()返回的是dict_keys,不是list,所以不能用
+    # myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    # next() 返回迭代器的下一个项目 next(iterator[, default])
+    firstStr = next(iter(myTree))
+    # 获取下一个字典
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        # 测试该结点是否为字典，如果不是字典，代表此节点为叶子结点
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        # 更新最深层数
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    # 返回决策树的层数
+    return maxDepth
+
+
+"""
+函数说明：绘制结点
+
+Parameters:
+    nodeTxt - 结点名
+    centerPt - 文本位置
+    parentPt - 标注的箭头位置
+    nodeType - 结点格式
+    
+Returns:
+    None
+    
+Modify:
+    2020-05-25
+"""  
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    # 定义箭头格式
+    arrow_args = dict(arrowstyle="<-")
+    # 绘制结点createPlot.ax1创建绘图区
+    # annotate是关于一个数据点的文本
+    # nodeTxt为要显示的文本，centerPt为文本的中心点，箭头所在的点，parentPt为指向文本的点
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', 
+                            xytext=centerPt, textcoords='axes fraction',
+                            va='center', ha='center', bbox=nodeType, 
+                            arrowprops=arrow_args)
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    """
+    函数说明：标注有向边属性值
+
+    Parameters:
+        cntrPt、parentPt - 用于计算标注位置
+        txtString - 标注内容
+        
+    Returns:
+        None
+        
+    Modify:
+        2020-05-25
+    """ 
+    # 计算标注位置（箭头起始位置的中点处）
+    xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    """
+    函数说明：绘制决策树
+
+    Parameters:
+        myTree - 决策树（字典）
+        parentPt - 标注的内容
+        nodeTxt - 结点名
+        
+    Returns:
+        None
+        
+    Modify:
+        2020-05-25
+    """ 
+    # 设置结点格式boxstyle为文本框的类型，sawtooth是锯齿形，fc是边框线粗细
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+    # 设置叶结点格式
+    leafNode = dict(boxstyle="round4", fc="0.8")
+    # 获取决策树叶结点数目，决定了树的宽度
+    numLeafs = getNumLeafs(myTree)
+    # 获取决策树层数
+    depth = getTreeDepth(myTree)
+    # 下个字典
+    firstStr = next(iter(myTree))
+    # 中心位置
+    cntrPt = (plotTree.xoff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yoff)
+    # 标注有向边属性值
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    # 绘制结点
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    # 下一个字典，也就是继续绘制结点
+    secondDict = myTree[firstStr]
+    # y偏移
+    plotTree.yoff = plotTree.yoff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+        if type(secondDict[key]).__name__ == 'dict':
+            # 不是叶结点，递归调用继续绘制
+            plotTree(secondDict[key], cntrPt, str(key))
+        # 如果是叶结点，绘制叶结点，并标注有向边属性值
+        else:
+            plotTree.xoff = plotTree.xoff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xoff, plotTree.yoff), cntrPt, leafNode)
+            plotMidText((plotTree.xoff, plotTree.yoff), cntrPt, str(key))
+    plotTree.yoff = plotTree.yoff + 1.0 / plotTree.totalD
+
+
+def createPlot(inTree):
+    """
+    函数说明：创建绘图面板
+
+    Parameters:
+        inTree - 决策树（字典）
+        
+    Returns:
+        None
+        
+    Modify:
+        2020-05-25
+    """ 
+    # 创建fig
+    fig = plt.figure(1, facecolor="white")
+    # 清空fig
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    # 去掉x、y轴
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    # 获取决策树叶结点数目
+    plotTree.totalW = float(getNumLeafs(inTree))
+    # 获取决策树层数
+    plotTree.totalD = float(getTreeDepth(inTree))
+    # x偏移
+    plotTree.xoff = -0.5 / plotTree.totalW
+    plotTree.yoff = 1.0
+    # 绘制决策树
+    plotTree(inTree, (0.5, 1.0), '')
+    # 显示绘制结果
+    plt.show()
+
+
+def classify(inputTree, featLabels, testVec):
+    """
+    函数说明：使用决策树分类
+
+    Parameters:
+        inputTree - 已经生成的决策树
+        featLabels - 存储选择的最优特征标签
+        testVec - 测试数据列表，顺序对应最优特征标签
+        
+    Returns:
+        classLabel - 分类结果
+        
+    Modify:
+        2020-05-25
+    """ 
+    # 获取决策树结点
+    firstStr = next(iter(inputTree))
+    # 下一个字典
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+  
+def storeTree(inputTree, filename):
+    """
+    函数说明：存储决策树
+
+    Parameters:
+        inputTree - 已经生成的决策树
+        filename - 决策树的存储文件名
+        
+    Returns:
+        None
+        
+    Modify:
+        2020-05-25
+    """ 
+    with open(filename, 'wb') as fw:
+        pickle.dump(inputTree, fw)
+
+
+def grabTree(filename):
+    """
+    函数说明：读取决策树
+
+    Parameters:
+        filename - 决策树的存储文件名
+        
+    Returns:
+        pickle.load(fr) - 决策树字典
+        
+    Modify:
+        2020-05-25
+    """ 
+    fr = open(filename, 'rb')
+    return pickle.load(fr)
+
+
+def main():
+    dataSet, features = createDataSet()
+    featLabels = []
+    myTree = createTree(dataSet, features, featLabels)
+    # storeTree(myTree, 'classifierStorage.txt')
+    # myTree = grabTree('classifierStorage.txt')
+    # print(myTree)
+    # 测试数据
+    testVec = [0, 1, 1, 1]
+    result = classify(myTree, featLabels, testVec)
+    if result == 'yes':
+        print('放贷')
+    if result == 'no':
+        print('不放贷')
+    print(myTree)
+    createPlot(myTree)
+    # print(dataSet)
+    # print(calcShannonEnt(dataSet))
+    print("最优特征索引值:" + str(chooseBestFeatureToSplit(dataSet)))
+
+
 if __name__ == "__main__":
-    pass
+    main()
